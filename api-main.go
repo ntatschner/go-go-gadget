db03ad95f42aecee67c7772d561c536544229f24
Nigel Tatschner <ntatschner@gmail.com>
2023-07-10 10:58:45 +0100

    =Initial setup and first get query

---
+++ api-main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Definition of available services
+type service struct {
+	Name          string `json:"name"`
+	ServiceName   string `json:"servicename"`
+	SecurityLevel int    `json:"securitylevel"`
+	ServiceType   string `json:"servicetype"`
+}
+
+// For testing purposes i'm creating in-memory data instead of retrieving from database
+var services = []service{
+	{
+		Name:          "Proxmox",
+		ServiceName:   "proxmox",
+		SecurityLevel: 5,
+		ServiceType:   "HyperVisor",
+	},
+	{
+		Name:          "Voyager Docker Host",
+		ServiceName:   "voyager-docker-host",
+		SecurityLevel: 5,
+		ServiceType:   "ContainerHost",
+	},
+	{
+		Name:          "Enterprise Docker Host",
+		ServiceName:   "enterprise-dicker-host",
+		SecurityLevel: 5,
+		ServiceType:   "ContainerHost",
+	},
+	{
+		Name:          "UniFi Dream Machine Pro",
+		ServiceName:   "unifi-dream-machine-pro",
+		SecurityLevel: 5,
+		ServiceType:   "Networking",
+	},
+}
+
+// Main package function
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getServices)
+
+	router.Run("localhost:8080")
+}
+
+// getServices responds with the list of all services as JSON.
+func getServices(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, services)
+}
