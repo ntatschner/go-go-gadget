429f03676915de03f07711de4a204b2103b20a8d
Nigel Tatschner <ntatschner@gmail.com>
2023-07-10 23:56:21 +0100

    =added the start of postgress database intergration, have connections and table validation basics in

--- src/api-main.go
+++ src/api-main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
-
-	"os"
+	_ "github.com/lib/pq"
 )
 
 // Package level variables
@@ -49,10 +51,49 @@ var services = []service{
 
 // Main package function
 func main() {
+	if os.Getenv("POSTGRESHOST") == "" {
+		log.Fatal("Postgres host not specified, ensure POSTGRESHOST is specified on container run")
+		os.Exit(1)
+	}
+	// establish database connection
+	databaseConnection, err := connectDatabase(os.Getenv("POSTGRESHOST"), os.Getenv("DATABASE"), os.Getenv("POSTGRESUSERNAME"), os.Getenv("POSTGRESPASSWORD"))
+	if err != nil {
+		log.Fatalf("Failed to connect to the database. Error: %p", err)
+		os.Exit(1)
+	}
+	// Test if require tables are present
+	validateTables(databaseConnection, "Services")
+	// prepare and launch http server
 	router := gin.Default()
 	router.GET("/services", getServices)
 
 	router.Run(":" + listenOnPort)
+}
+
+// Database connection functions
+func connectDatabase(URL string, databasename string, username string, password string) (db *sql.DB, exception error) {
+	connStr := "user=username dbname=databasename sslmode=verify-full"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+// Ensure required tables are present in the database and create the table if not
+func validateTables(dbconnection *sql.DB, tablename ...string) (servicesTable *sql.Rows, exception error) {
+	servicesTable, err := dbconnection.Query(`
+		CREATE TABLE IF NOT EXISTS Services (
+		Name VARCHAR(50) NOT NULL,
+		ServiceName VARCHAR(50) NOT NULL,
+		SecurityLevel INT NOT NULL,
+		ServiceType VARCHAR(50) NOT NULL
+		);`, tablename)
+	if err != nil {
+		log.Fatal("Failed to run validation, ensure the user has rights to the database")
+		os.Exit(2)
+	}
+	return
 }
 
 // getServices responds with the list of all services as JSON.
